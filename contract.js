51c1ac76a3084f750478127d8035fe9df21385ea
Alvin Reyes <me@areyes.onl>
2022-02-05 14:02:22 -0500

    first commit

---
+++ contract.js
@@ -0,0 +1,70 @@
+const forwarderOrigin = 'http://localhost:9010'
+
+const initialize = () => {
+  //Basic Actions Section
+  const onboardButton = document.getElementById('connectButton');
+  const getAccountsButton = document.getElementById('getAccounts');
+  const getAccountsResult = document.getElementById('getAccountsResult');
+  const oasisNetworkTa = document.getElementById('input_9')
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  //We create a new MetaMask onboarding object to use in our app
+  const onboarding = new MetaMaskOnboarding({ forwarderOrigin });
+
+//This will start the onboarding proccess
+  const onClickInstall = () => {
+    onboardButton.innerText = 'Onboarding in progress';
+    onboardButton.disabled = true;
+    //On this object we have startOnboarding which will start the onboarding process for our end user
+    onboarding.startOnboarding();
+  };
+
+  const onClickConnect = async () => {
+    try {
+      // Will open the MetaMask UI
+      // You should disable this button while the request is pending!
+      await ethereum.request({ method: 'eth_requestAccounts' });
+      //This will check to see if the user has connected their MetaMask wallet
+      const accounts = await ethereum.request({ method: 'eth_accounts' });
+      //We take the first address in the array of addresses and display it
+      oasisNetworkTa.value = accounts[0] || 'Not able to get accounts'; 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if Metmask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      onboardButton.innerText = 'Click here to install MetaMask!';
+      //When the button is clicked we call th is function
+      onboardButton.onclick = onClickInstall;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    } else {
+      //If MetaMask is installed we ask the user to connect to their wallet
+      onboardButton.innerText = 'Connect Wallet';
+      //When the button is clicked we call this function to connect the users MetaMask Wallet
+      onboardButton.onclick = onClickConnect;
+      //The button is now disabled
+      onboardButton.disabled = false;
+    }
+  };
+  MetaMaskClientCheck(); 
+}
+
+const changeAccount = async () => {
+  const oasisNetworkTa = document.getElementById('input_9')
+  const accounts = await ethereum.request({ method: 'eth_accounts' });
+  oasisNetworkTa.value = accounts[0] || 'Not able to get accounts';
+}
+
+window.addEventListener('DOMContentLoaded', initialize)
+window.ethereum.on('accountsChanged', changeAccount)
